e81d730e8dc63951481978b8100310f25d13bb35
Sung Paik <sung.paik@codeimmersives.com>
2020-02-24 16:24:21 -0500

    connected app to local server

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'house'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/users', usersRouter);
+app.use('/', indexRouters);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(next, req, resxt) {
+router.get('/', function(req, res, next) {
   res.render('/index', { title: 'Express' });
 });
 
